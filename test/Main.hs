7f1938eef952ab49d047ec6c8943b7fbae9a558a
Brad Neimann <brad.neimann@hotmail.com>
2021-01-09 19:33:19 +1100

    Add tests for 'tokeniseWord'

--- test/Main.hs
+++ test/Main.hs
@@ -4,9 +4,11 @@ import Test.Tasty (defaultMain, testGroup)
 
 import qualified MultiZipper.Tests
 import qualified SoundChange.Apply.Tests
+import qualified SoundChange.Parse.Tests
 
 main :: IO ()
 main = defaultMain $ testGroup "Tests"
     [ MultiZipper.Tests.tests
     , SoundChange.Apply.Tests.tests
+    , SoundChange.Parse.Tests.tests
     ]
