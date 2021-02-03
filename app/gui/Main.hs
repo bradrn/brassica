c0aa424aca2b886ccb180142a14402683a27e911
Brad Neimann <brad.neimann@hotmail.com>
2021-02-03 14:08:31 +1100

    Add output highlighting

--- app/gui/Main.hs
+++ app/gui/Main.hs
@@ -1,9 +1,12 @@
 {-# LANGUAGE LambdaCase        #-}
 {-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE TupleSections     #-}
+{-# LANGUAGE TypeApplications  #-}
 
 module Main where
 
 import Data.Bifunctor (bimap)
+import Data.Functor ((<&>))
 import GHC.IO.Encoding (setLocaleEncoding, utf8)
 import System.Environment (getArgs)
 import System.IO (hSetBuffering, stdout, BufferMode(LineBuffering))
@@ -13,8 +16,10 @@ import qualified Graphics.UI.Threepenny as UI
 import Graphics.UI.Threepenny.Core
 import Foreign.JavaScript (IsHandler, JSObject)
 
-import SoundChange
+import SoundChange.Apply (applyStr)
+import SoundChange.Category (values)
 import SoundChange.Parse
+import SoundChange.Types (Grapheme, Rule)
 
 main :: IO ()
 main = do
@@ -35,6 +40,10 @@ setup window = do
     applyBtn <- getElementById' "applyBtn"
     out      <- getElementById' "out"
 
+    -- A 'Behaviour' to keep track of the previous output.
+    (prevOutE, setPrevOut) <- liftIO $ newEvent @[[[Grapheme]]]
+    prevOut <- stepper [] prevOutE
+
     on UI.keydown cats $ const $ do
         catsText  <- lines <$> get value cats
         rehighlight catsText
@@ -46,9 +55,20 @@ setup window = do
 
         let cats = parseCategoriesSpec catsText
             rules = parseRules cats rulesText
-            results = tokeniseAndApplyRules cats rules <$> wordsText
 
-        element out # set value (unlines results)
+            results = fmap (applyRulesWithChanges rules) . tokeniseWords (values cats) <$> wordsText
+
+        prevResults <- liftIO $ currentValue prevOut
+        liftIO $ setPrevOut $ (fmap.fmap) fst results
+
+        results' <- getHlMode <&> \mode -> fmap unwords $
+            zipWith2' results prevResults [] $ \(word, hasBeenAltered) prevWord ->
+                case mode of
+                    HlNone -> concat word
+                    HlRun -> surroundBold (word /= prevWord) $ concat word
+                    HlInput -> surroundBold hasBeenAltered $ concat word
+
+        element out # set html (unlines results')
 
     _ <- exportAs "openRules" $ runUI window . openRules cats rules
     _ <- exportAs "saveRules" $ runUI window . saveRules cats rules
@@ -60,6 +80,34 @@ setup window = do
     getElementById' i = getElementById window i >>= \case
         Nothing -> error "Tried to get nonexistent ID"
         Just el -> return el
+
+    surroundBold False w = w
+    surroundBold True  w = "<b>" ++ w ++ "</b>"
+
+    zipWith2' :: [[a]] -> [[b]] -> b -> (a -> b -> c) -> [[c]]
+    zipWith2' ass bss bd f = zipWith' ass bss [] $ \as bs -> zipWith' as bs bd f
+
+    zipWith' :: [a] -> [b] -> b -> (a -> b -> c) -> [c]
+    zipWith' []      _     _ _ = []
+    zipWith' as     []     bd f = fmap (`f` bd) as
+    zipWith' (a:as) (b:bs) bd f = f a b : zipWith' as bs bd f
+
+applyRulesWithChanges :: [Rule] -> [Grapheme] -> ([Grapheme], Bool)
+applyRulesWithChanges = flip (go . (,False))
+  where
+    go gs [] = gs
+    go (gs,w) (r:rs) =
+        let gs' = applyStr r gs
+        in go (gs', w || (gs/=gs')) rs
+
+data HlMode = HlNone | HlRun | HlInput deriving (Show)
+
+getHlMode :: UI HlMode
+getHlMode = callFunction (ffi @(JSFunction String) "$('input[name=hl-options]:checked').val()") >>= \case
+    "hl-none"  -> return HlNone
+    "hl-run"   -> return HlRun
+    "hl-input" -> return HlInput
+    _ -> error "unexpected highlighting mode"
 
 rehighlight :: [String] -> UI ()
 rehighlight catsText =
