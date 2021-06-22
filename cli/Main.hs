c85b3491905dae7099ceca186efdd7c19a82399a
Brad Neimann <brad.neimann@hotmail.com>
2021-06-23 00:38:47 +1000

    Rewrite CLI executable to be better organised

--- cli/Main.hs
+++ cli/Main.hs
@@ -1,31 +1,56 @@
+{-# LANGUAGE LambdaCase   #-}
 {-# LANGUAGE ViewPatterns #-}
 
 module Main where
 
+import Control.Exception (Exception)
+import Data.Bifunctor (first, bimap)
 import System.Environment (getArgs)
-import System.IO
 
+import Conduit
 import qualified Data.ByteString as B
 import Data.Text (pack, unpack)
-import Data.Text.Encoding (decodeUtf8, encodeUtf8)
+import Data.Text.Encoding (decodeUtf8)
 
 import SoundChange
 import SoundChange.Parse
+import SoundChange.Types (Grapheme)
+import Data.Conduit.Combinators (iterM)
 
 
 main :: IO ()
 main = do
-    [inputFile, wordsFile, outFile] <- getArgs
-    changesText <- readFile inputFile
-
-    wordsText <- (unpack . decodeUtf8) <$> B.readFile wordsFile
+    [inputFile] <- getArgs
+    changesText <- unpack . decodeUtf8 <$> B.readFile inputFile
 
     case parseSoundChanges changesText of
         Left err ->
             putStrLn $ errorBundlePretty err
-        Right rules -> do
-            case tokeniseAnd applyChanges rules wordsText of
-                Left err ->
-                    putStrLn $ errorBundlePretty err
-                Right outWordsText ->
-                    B.writeFile outFile $ encodeUtf8 $ pack $ detokeniseWords outWordsText
+        Right rules ->
+            runConduit $ processWords $ first errorBundlePretty . tokeniseAnd applyChanges rules
+
+processWords
+    :: (MonadIO m, MonadThrow m)
+    => (String -> Either String [Component [Grapheme]])
+    -> ConduitT () Void m ()
+processWords evolve = stdinC
+    .| iterM (liftIO . print)
+    .| decodeUtf8C
+    .| linesUnboundedC
+    .| iterM (liftIO . print)
+    .| mapC (bimap ParseException (pack . detokeniseWords) . evolve . unpack)
+    .| throwOnLeft
+    .| iterM (liftIO . print)
+    .| unlinesC
+    .| encodeUtf8C
+    .| stdoutC
+  where
+    throwOnLeft :: (MonadThrow m, Exception e) => ConduitT (Either e r) r m ()
+    throwOnLeft = awaitForever $ \case
+        Left e -> throwM e
+        Right r -> yield r
+
+newtype ParseException = ParseException String
+    deriving Show
+
+instance Exception ParseException
