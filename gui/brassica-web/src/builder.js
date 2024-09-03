f4f9419eb00ba429fee0b2655e7ffbfbb1ea958a
Brad Neimann <brad.neimann@hotmail.com>
2024-09-03 18:38:30 +1000

    Add file buttons to paradigm builder also

--- gui/brassica-web/src/builder.js
+++ gui/brassica-web/src/builder.js
@@ -37,6 +37,7 @@ function buildParadigm(paradigm, words, separateLines) {
 Split(["#paradigm-div", "#roots-div", "#output-div"]);
 
 const form = document.getElementById("brassica-form");
+const outputDiv = document.getElementById("output");
 
 form.addEventListener("submit", (event) => {
     event.preventDefault();
@@ -48,7 +49,7 @@ form.addEventListener("submit", (event) => {
 
     const output = buildParadigm(paradigm, roots, separateLines);
     console.log(output);
-    document.getElementById("output").innerHTML = output;
+    outputDiv.innerHTML = output;
 });
 
 const blurb = document.getElementById("blurb");
@@ -59,4 +60,74 @@ blurb.addEventListener("toggle", (event) => {
     } else {
         blurbHeader.innerHTML = "Click to open";
     }
+});
+
+// adapted from https://stackoverflow.com/a/33542499/
+function save(filename, data) {
+    const blob = new Blob([data]);
+    const elem = window.document.createElement('a');
+    const url = window.URL.createObjectURL(blob);
+    elem.href = url;
+    elem.download = filename;
+    elem.style.display = 'none';
+    document.body.appendChild(elem);
+    elem.click();
+    document.body.removeChild(elem);
+    URL.revokeObjectURL(url);
+}
+
+const paradigmArea = document.getElementById("paradigm");
+const rootsArea = document.getElementById("roots");
+
+document.getElementById("download-paradigm").addEventListener("click", (event) => {
+    event.preventDefault();
+    save("paradigm.bsc", paradigmArea.value);
+});
+document.getElementById("download-roots").addEventListener("click", (event) => {
+    event.preventDefault();
+    save("roots.lex", rootsArea.value);
+});
+
+const inputFileParadigm = document.getElementById("input-file-paradigm");
+inputFileParadigm.addEventListener("change", (event) => {
+    const file = inputFileParadigm.files[0];
+    if (file) {
+        const reader = new FileReader();
+        reader.onload = (e) => {
+            paradigmArea.value = e.target.result;
+        };
+        reader.readAsText(file);
+    }
+});
+document
+    .getElementById("open-paradigm")
+    .addEventListener("click", (event) => inputFileParadigm.click());
+
+const inputFileRoots = document.getElementById("input-file-roots");
+inputFileRoots.addEventListener("change", (event) => {
+    const file = inputFileRoots.files[0];
+    if (file) {
+        const reader = new FileReader();
+        reader.onload = (e) => {
+            rootsArea.value = e.target.result;
+        };
+        reader.readAsText(file);
+    }
+});
+document
+    .getElementById("open-roots")
+    .addEventListener("click", (event) => inputFileRoots.click());
+
+document.getElementById("select-all-paradigm").addEventListener("click", (event) => {
+    paradigmArea.select();
+});
+document.getElementById("select-all-roots").addEventListener("click", (event) => {
+    rootsArea.select();
+});
+document.getElementById("select-all-output").addEventListener("click", (event) => {
+    // see https://stackoverflow.com/a/1173319
+    var range = document.createRange();
+    range.selectNode(outputDiv);
+    window.getSelection().removeAllRanges();
+    window.getSelection().addRange(range);
 });
